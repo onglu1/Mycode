12c86442cedb487c96340c3ec132f2e9868895af
onglu1 <34936104+onglu1@users.noreply.github.com>
2019-01-25 10:24:47 +0800

    Update 3203.cpp

--- luogu/3203.cpp
+++ luogu/3203.cpp
@@ -7,27 +7,33 @@ int read(){
 	while(c=getchar(), isdigit(c))num=num*10+c-'0';
 	return num*f;
 }
+int rl[N],id[N],cnt=-1;
 int n,m,fa[N],dep[N],siz[N],top[N],son[N];
 set<int>S[N];
-set<int>::iterator it;
-void Add_Son(int u,int v){S[u].insert(u);fa[v]=u;}
+void Add_Son(int u,int v){S[u].insert(v);fa[v]=u;}
 void Delete_Son(int u,int v){
-	set<int>::iterator itt;
-	itt=S[u].find(v);
-	if(itt!=S[u].end())S[u].erase(itt);
+	set<int>::iterator it;
+	it=S[u].find(v);
+	if(it!=S[u].end())S[u].erase(it);
 	fa[v]=0;
 }
 void dfs1(int x,int deep){
+	set<int>::iterator it;
 	dep[x]=deep;siz[x]=1;
+	//cout<<"Outside:"<<x<<endl;
 	for(it=S[x].begin();it!=S[x].end();it++){
 		int y=*it;
+		//cout<<"Inside:"<<y<<endl;
 		dfs1(y,deep+1);
 		siz[x]+=siz[y];
 		if(siz[y]>=siz[son[x]])son[x]=y;
 	}
+	//cout<<x<<endl;
 }
 void dfs2(int x,int ltp){
+	set<int>::iterator it;
 	top[x]=ltp;
+	id[x]=++cnt;rl[cnt]=x;
 	if(son[x])dfs2(son[x],ltp);
 	for(it=S[x].begin();it!=S[x].end();it++){
 		int y=*it;
@@ -35,16 +41,44 @@ void dfs2(int x,int ltp){
 		dfs2(y,y);
 	}
 }
+void Ask(int x){
+	int ans=0;
+ 	while(fa[x]!=0){
+		ans+=id[x]-id[top[x]]+1;
+		x=fa[top[x]];
+	}
+	printf("%d\n",ans-1);
+}
+void Modify(int x,int y){
+	int bg=id[x];
+	for(int i=id[x]+1;top[rl[i]]==top[x];i++)
+		top[rl[i]]=x;
+	top[x]=x;
+	Delete_Son(fa[x],x);
+	fa[x]=y;
+	Add_Son(fa[x],x);
+}
 int main()
 {
+	//freopen("data.in","r",stdin);
+	//freopen("data.out","w",stdout);
 	n=read();
 	for(int i=1;i<=n;i++){
 		int x=read();
-		Add_Son((i+x>n)?(n+1):(i+x),i);
+		Add_Son((i+x>n)?n+1:(i+x),i);
 	}
-	
-	//dfs1(n+1,0);dfs2(n+1,n+1);
-	//m=read();
+	/*for(it=S[3].begin();it!=S[3].end();it++){
+		int y=*it;
+		cout<<y<<endl;
+	}*/
+	dfs1(n+1,0);dfs2(n+1,n+1);
+	//cout<<dep[1]<<endl;
+	m=read();
+	for(int i=1;i<=m;i++){
+		int opt=read(),x=read(),y;
+		if(opt==1){Ask(x+1);}
+		if(opt==2){y=read();Modify(x+1,(x+y+1>n)?n+1:(x+y+1));}
+	}
 	return 0;
 }
 /*
@@ -68,7 +102,6 @@ Splay？
 左兄弟右儿子转二叉树还是不现实
 发现很多的平衡树都是二叉结构的。。
 或许不能用平衡树写。。
-
 继续用树剖想想看。。
 静态的话直接找重链跳根然后深度求和就可以了
 动态的话。。
@@ -84,10 +117,4 @@ Splay？
 每次询问输出深度
 修改的话，在每个点开一个set存它的儿子集合
 修改就挺简单了。
- 
 */ 
-
-
-
-
-
