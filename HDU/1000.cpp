44b38d600f709638879192269884282c40d5900d
onglu1 <34936104+onglu1@users.noreply.github.com>
2021-03-01 11:18:08 +0800

    Update 1000.cpp

--- HDU/1000.cpp
+++ HDU/1000.cpp
@@ -1,1 +1,17 @@
-
+#include <bits/stdc++.h>
+#define Mid ((l + r) >> 1)
+#define lson (rt << 1)
+#define rson (rt << 1 | 1)
+using namespace std;
+int read(){
+	char c; int num, f = 1;
+	while(c = getchar(),!isdigit(c)) if(c == '-') f = -1; num = c - '0';
+	while(c = getchar(), isdigit(c)) num = num * 10 + c - '0';
+	return f * num;
+}
+signed main()
+{
+	int a, b;
+	while(cin >> a >> b) cout << a + b << endl;
+	return 0;
+}
